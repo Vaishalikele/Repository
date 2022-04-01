cb3f7f289f57de913470d3d3bca41b9eda9e1728
vaish <vaish@LAPTOP-PNF9V9VN>
2022-04-01 17:18:38 +0530

    new add

---
+++ firstbootapp/src/main/java/fi/spring/firstbootapp/FirstbootappApplication.java
@@ -0,0 +1,13 @@
+package fi.spring.firstbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstbootappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstbootappApplication.class, args);
+	}
+
+}
---
+++ firstbootapp/src/test/java/fi/spring/firstbootapp/FirstbootappApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.spring.firstbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstbootappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
